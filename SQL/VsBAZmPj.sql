219e0823db61beac48becd3aa14ae5ce7e566fcd
jiadonglei <jiadongleiboy@163.com>
2016-06-04 10:44:03 +0800

    1、添加病案筛选数据表VsPJBAOA 2、修改VsBAZmPj

---
+++ SQL/VsBAZmPj.sql
@@ -0,0 +1,32 @@
+USE [batj2005]
+GO
+
+/****** Object:  Table [dbo].[VsBAZmPj]    Script Date: 05/25/2016 16:24:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[VsBAZmPj](
+	[CH0A00] [varchar](10) NOT NULL,
+	[Code] [varchar](15) NOT NULL,
+	[Score] [numeric](5, 2) NULL,
+	[Remark] [varchar](1000) NULL,
+ CONSTRAINT [PK_VsBAZmPj] PRIMARY KEY CLUSTERED 
+(
+	[CH0A00] ASC,
+	[Code] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
